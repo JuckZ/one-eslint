bba9a3e0d468b53e26a3ea243abbda9c103f6f0e
Juck <juckz@foxmail.com>
2023-09-08 18:08:35 +0800

    feat: 1.add Identifier Test

--- tests/lib/rules/eslint-rule-one.js
+++ tests/lib/rules/eslint-rule-one.js
@@ -19,12 +19,18 @@ const rule = require("../../../lib/rules/eslint-rule-one"),
 const ruleTester = new RuleTester();
 ruleTester.run("eslint-rule-one", rule, {
   valid: [
+    'bar',
+    'baz',
     {
       code: "console.xxx('failed')",
     },
   ],
 
   invalid: [
+    {
+      code: 'foo',
+      errors: [{ message: "Avoid using variables named 'foo'", type: "Identifier" }],
+    },
     {
       code: "console.log('foo')",
       errors: [{ message: "Avoid using console.log", type: "CallExpression" }],
